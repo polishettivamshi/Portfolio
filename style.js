e57dfaf7a566e4e2ca00b12cac6673f81975586d
Polishetti Vamshi <109717338+polishettivamshi@users.noreply.github.com>
2023-05-22 11:09:10 +0530

    style.js

--- style.js
+++ style.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing",{
-		strings: ["Java Developer ", "Coding Enthusiast", " Backend Developer"],
+		strings: ["Aspiring Developer ", "Coding Enthusiast", " Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Java Developer", "Coding Enthusiast", "Backend Developer"],
+        strings: ["Aspiring Developer", "Coding Enthusiast", "Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
